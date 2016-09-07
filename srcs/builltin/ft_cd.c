1bdb239198f10d2ab3dff75dcae3f2ee9d49195b
Samuel BELINE <sbeline@e1r11p23.42.fr>
2016-09-07 20:28:00 +0200

    cd_to_correct

--- srcs/builltin/ft_cd.c
+++ srcs/builltin/ft_cd.c
@@ -7,21 +7,20 @@ int			ft_cd(t_data *data, t_llist *env, t_memory *memory)
 	int		i;
 
 	i = 1;
-	exit(1);
+
     if (data->option[i] == NULL)
 	{
+		ft_putendl("in");
 		chdir(search_env(env, "HOME="));
 		ft_unsetenv(data, env, memory);
 		export_var(&env, ft_strjoin("PWD=", search_env(env, "HOME=")));
 	}
-	else
-	{
-		while (data->option[i])
-			i++;
-		if (i > 2)
-			return (48);
-		if (is_dir(data->option[i]))
-			;
-	}
+	exit(1);
+	while (data->option[i])
+		i++;
+	if (i > 2)
+		return (48);
+	if (is_dir(data->option[i]))
+		;
 	return (1);
 }
